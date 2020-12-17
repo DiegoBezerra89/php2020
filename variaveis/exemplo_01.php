852d47e4cd48e3a5808e362273aba6306a5c4b2c
Diego Bezerra Martins <diegobmartins21@gmail.com>
2020-12-16 22:24:33 -0300

    variaveis 01

---
+++ variaveis/exemplo_01.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = "Diego";
+
+//echo $nome;
+//Diego
+
+var_dump($nome);
+//C:\wamp64\www\variaveis\exemplo_01.php:8:string 'Diego' (length=5)
+
+?>
---
+++ variaveis/exemplo_02.php
@@ -0,0 +1,27 @@
+<?php
+  $anoNascimento = 1989;
+  
+  echo$anoNascimento , "<br/>";
+  
+  $nome = "Diego";
+  
+  //comentario
+  echo$nome;
+
+  //exit; //para a execução do código
+  
+  //limpando variáveis
+  unset($nome);
+
+  if(!isset($nome)){ //isset: existe? (true ou false)
+    //nomeCompleto não existe mais
+    echo$nome;
+  };
+
+  $nome = "Diego";
+  $sobrenome = "Bezerra";
+  //concatenação por meio do ponto .
+  $nomeCompleto = $nome . " " . $sobrenome;
+
+  echo $nomeCompleto;
+?>
